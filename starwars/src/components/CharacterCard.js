f324011388f08657f1c0a98c93c801218b732c3f
Sean Birmingham <seannbirmingham98@gmail.com>
2019-11-22 22:40:26 -0500

    styled cards using styled-components

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,13 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const CharacterCardContainer = styled.div`
+    background: black;
+    color: #FFE81F;
+    width: 40%;
+    margin: 2% auto;
+    padding: 5px 5px;
+    border-radius: 20px;
+    box-shadow: 0 4px 8px 0 #000;
+`
 
 const CharacterCard = props => {
     return (
-        <div>
+        <CharacterCardContainer>
             <h2>{props.character.name}</h2>
             <p>Birth year: {props.character.birth_year}</p>
             <p>Gender: {props.character.gender}</p>
             <p>Hair color: {props.character.hair_color}</p>
-        </div>
+        </CharacterCardContainer>
     );
 };
 
